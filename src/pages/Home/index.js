ec51f04f401157a60a01de79d37b9a1eb634e201
Pablo Suarez <pablooscarsuarez@gmail.com>
2019-09-17 15:56:34 -0300

    configuration finished

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+export default function home() {
+  return (
+    <Container>
+      <Typography> HOME </Typography>
+    </Container>
+  );
+}
